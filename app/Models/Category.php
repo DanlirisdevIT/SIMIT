62ea2e7650b9cebcc5d3df54192aef80c6294669
DanlirisdevIT <danlirisdevit@gmail.com>
2021-12-14 09:45:42 +0700

    adding asset menu
    menambahkan menu CRUD asset

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Asset;
 
 class Category extends Model
 {
@@ -11,5 +12,11 @@ class Category extends Model
 
     public $timestamps = false;
 
+    protected $primaryKey = 'id';
+
     protected $fillable = ['category_name', 'category_type'];
+
+    public function assets(){
+        return $this->hasOne(Asset::class, 'category_id', 'id');
+    }
 }
--- app/Models/Manufacture.php
+++ app/Models/Manufacture.php
@@ -4,11 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Asset;
 
 class Manufacture extends Model
 {
     use HasFactory;
+
     public $timestamps = false;
 
+    protected $primaryKey = 'id';
+
     protected $fillable = ['manufactureName', 'Url', 'supportEmail', 'supportPhone', 'image'];
+
+    public function assets(){
+        return $this->hasOne(Asset::class, 'manufacture_id', 'id');
+    }
 }
